c3e4b6245c47290e6bc94127db955553c3072533
Mauli Kashid <kashidnamdevmauli777@gmail.com>
2025-06-10 21:52:09 +0530

    second commit

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin:['http://localhost:5173'],
+    origin:['https://favoritefood-three.vercel.app','https://favoritefood-admin.vercel.app/'],
     credentials: true,
 }))
 connectToDb()
