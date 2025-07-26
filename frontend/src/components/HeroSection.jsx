116ad1bcc939369c0a68d6cd05b3fd3a4a6f9f19
Mauli Kashid <kashidnamdevmauli777@gmail.com>
2025-07-26 10:28:05 +0530

    tenth commit

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -1,10 +1,8 @@
-import { assets } from '../assets/assets'
-
 const HeroSection = () => {
   return (
     <div className='relative '>
-      <div className="img relative z-1">
-        <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxBOklOQD4v4QChxo3sUS95NN34zI037jLgA&s"} alt="" />
+      <div className="img relative z-1 ">
+        <img className='rounded-xl w-full h-full' src={"https://asliceofpi.ca/wp-content/uploads/2017/04/17.jpg"} alt="" />
       </div>
       <div className="content absolute bottom-3 left-5 z-5 text-white">
         <h1 className='text-2xl mb-2 font-bold lg:text-6xl sm:text-4xl'>Order Your <br /> Favourite Food <br /> Here</h1>
